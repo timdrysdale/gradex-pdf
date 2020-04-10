74a907bbb084bebf86cb00d69eec4411de8f7cb8
Tim Drysdale <timothy.d.drysdale@gmail.com>
2020-04-10 05:20:46 +0100

    boxes in right place for mark

--- cmd/ladder.go
+++ cmd/ladder.go
@@ -35,28 +35,28 @@ func getTextFieldsActionBar(pageNum int, verb string) []TextField {
 
 	var fields []TextField
 
-	fields = append(fields, TextField{ID: "pageOK", Bound: Box{TopLeft: Point{X: 20.3 * mm, Y: 47.0 * mm}, Width: 8 * mm, Height: 8 * mm}})
+	fields = append(fields, TextField{ID: "pageOK", Bound: Box{TopLeft: Point{X: 41.3 * mm, Y: (312 - 47.0) * mm}, Width: 8 * mm, Height: 8 * mm}})
 
 	// get Q box
 
 	qbox1 := getTextFieldsQ()
 
-	qbox1 = displaceTextFields(qbox1, 1.7*mm, 67.4*mm)
+	qbox1 = displaceTextFields(qbox1, 21.7*mm, (312-67.4)*mm)
 
 	for _, field := range qbox1 {
 		field.ID = fmt.Sprintf("page-%03d-Q1-%s-%s", pageNum, verb, field.ID)
 		fields = append(fields, field)
 	}
 
-	/*qbox2 := getTextFieldsQ()
-	qbox2 = displaceTextFields(qbox1, 1.7*mm, 137.1*mm)
+	qbox2 := getTextFieldsQ()
+	qbox2 = displaceTextFields(qbox2, 21.7*mm, (312-137.1)*mm)
 
 	for _, field := range qbox2 {
 		field.ID = fmt.Sprintf("page-%03d-Q2-%s-%s", pageNum, verb, field.ID)
 		fields = append(fields, field)
-	}*/
+	}
 
-	//fields = append(fields, TextField{ID: "taskOK", Bound: Box{TopLeft: Point{X: 19.8 * mm, Y: 286.4 * mm}, Width: 8 * mm, Height: 8 * mm}})
+	fields = append(fields, TextField{ID: "taskOK", Bound: Box{TopLeft: Point{X: (21.3 + 19.8) * mm, Y: (312 - 286.4) * mm}, Width: 8 * mm, Height: 8 * mm}})
 
 	return fields
 }
@@ -77,11 +77,11 @@ func getTextFieldsQ() []TextField {
 	mm := creator.PPMM
 
 	return []TextField{
-		{ID: "section", Bound: Box{TopLeft: Point{X: 17.6 * mm, Y: 6.3 * mm}, Width: 13 * mm, Height: 13 * mm}},
-		{ID: "number.left", Bound: Box{TopLeft: Point{X: 2.7 * mm, Y: 24.3 * mm}, Width: 13 * mm, Height: 13 * mm}},
-		{ID: "number.right", Bound: Box{TopLeft: Point{X: 17.5 * mm, Y: 24.3 * mm}, Width: 13 * mm, Height: 13 * mm}},
-		{ID: "mark.left", Bound: Box{TopLeft: Point{X: 2.7 * mm, Y: 45.2 * mm}, Width: 13 * mm, Height: 13 * mm}},
-		{ID: "mark.right", Bound: Box{TopLeft: Point{X: 17.5 * mm, Y: 45.2 * mm}, Width: 13 * mm, Height: 13 * mm}},
+		{ID: "section", Bound: Box{TopLeft: Point{X: 17.6 * mm, Y: -6.3 * mm}, Width: 13 * mm, Height: 13 * mm}},
+		{ID: "number.left", Bound: Box{TopLeft: Point{X: 2.7 * mm, Y: -24.3 * mm}, Width: 13 * mm, Height: 13 * mm}},
+		{ID: "number.right", Bound: Box{TopLeft: Point{X: 17.5 * mm, Y: -24.3 * mm}, Width: 13 * mm, Height: 13 * mm}},
+		{ID: "mark.left", Bound: Box{TopLeft: Point{X: 2.7 * mm, Y: -45.2 * mm}, Width: 13 * mm, Height: 13 * mm}},
+		{ID: "mark.right", Bound: Box{TopLeft: Point{X: 17.5 * mm, Y: -45.2 * mm}, Width: 13 * mm, Height: 13 * mm}},
 	}
 
 }
@@ -106,7 +106,7 @@ func addLadderTextFields(page *model.PdfPage, form *model.PdfAcroForm, ladder La
 
 	for _, box := range ladder.Boxes {
 		tfopt := annotator.TextFieldOptions{}
-		rect := []float64{box.Bound.TopLeft.X, box.Bound.TopLeft.Y, box.Bound.Width, box.Bound.Height}
+		rect := []float64{box.Bound.TopLeft.X, box.Bound.TopLeft.Y, box.Bound.TopLeft.X + box.Bound.Width, box.Bound.TopLeft.Y - box.Bound.Height}
 		textf, err := annotator.NewTextField(page, box.ID, rect, tfopt)
 		if err != nil {
 			panic(err)
